a4112a1855ea107c735529e1f820afd1112f2ff1
KyloJorgensen <kylo.j.jorgensen@gmail.com>
2016-08-08 10:46:53 -0600

    change main router to api. add mongoose

---
+++ server/api/main/main.controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var path = require('path');
+
+function MainController() {}
+
+MainController.prototype.getRoot = function(req, res) {
+
+    res.sendFile(path.join(__dirname, '../../../client/index.html'));
+
+}
+
+module.exports = MainController.prototype;
